b36aaadba46db951a091d39229763d5d82a73ba0
installer <installer@gmail.com>
2024-07-19 18:39:00 -0500

    Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed

--- config/waybar.nix
+++ config/waybar.nix
@@ -32,6 +32,7 @@ in
             "custom/hyprbindings"
             "custom/notification"
             "tray"
+            "custom/weather"
             "clock"
           ];
 
@@ -155,6 +156,14 @@ in
             on-click = "sleep 0.1 && task-waybar";
             escape = true;
           };
+          "custom/weather" : {
+          //shows the current weather and forecast
+          "tooltip" : true,
+          "format" : "{}",
+          "interval" : 30,
+          "exec" : "~/.config/waybar/scripts/waybar-wttr.py",
+          "return-type" : "json"
+          },
           "battery" = {
             states = {
               warning = 30;
@@ -181,97 +190,168 @@ in
         }
       ];
       style = concatStrings [
-        ''
+       ''
           * {
-            font-family: JetBrainsMono Nerd Font Mono;
-            font-size: 16px;
-            border-radius: 0px;
-            border: none;
-            min-height: 0px;
-          }
-          window#waybar {
-            background: rgba(0,0,0,0);
-          }
-          #workspaces {
-            color: #${config.stylix.base16Scheme.base00};
-            background: #${config.stylix.base16Scheme.base01};
-            margin: 4px 4px;
-            padding: 5px 5px;
-            border-radius: 16px;
-          }
-          #workspaces button {
-            font-weight: bold;
-            padding: 0px 5px;
-            margin: 0px 3px;
-            border-radius: 16px;
-            color: #${config.stylix.base16Scheme.base00};
-            background: linear-gradient(45deg, #${config.stylix.base16Scheme.base08}, #${config.stylix.base16Scheme.base0D});
-            opacity: 0.5;
-            transition: ${betterTransition};
-          }
-          #workspaces button.active {
-            font-weight: bold;
-            padding: 0px 5px;
-            margin: 0px 3px;
-            border-radius: 16px;
-            color: #${config.stylix.base16Scheme.base00};
-            background: linear-gradient(45deg, #${config.stylix.base16Scheme.base08}, #${config.stylix.base16Scheme.base0D});
-            transition: ${betterTransition};
-            opacity: 1.0;
-            min-width: 40px;
-          }
-          #workspaces button:hover {
-            font-weight: bold;
-            border-radius: 16px;
-            color: #${config.stylix.base16Scheme.base00};
-            background: linear-gradient(45deg, #${config.stylix.base16Scheme.base08}, #${config.stylix.base16Scheme.base0D});
-            opacity: 0.8;
-            transition: ${betterTransition};
-          }
-          tooltip {
-            background: #${config.stylix.base16Scheme.base00};
-            border: 1px solid #${config.stylix.base16Scheme.base08};
-            border-radius: 12px;
-          }
-          tooltip label {
-            color: #${config.stylix.base16Scheme.base08};
-          }
-          #window, #pulseaudio, #cpu, #memory, #disk, #network, #idle_inhibitor {
-            font-weight: bold;
-            margin: 4px 0px;
-            margin-left: 7px;
-            padding: 0px 18px;
-            background: #${config.stylix.base16Scheme.base04};
-            color: #${config.stylix.base16Scheme.base00};
-            border-radius: 24px 10px 24px 10px;
-          }
-          #custom-startmenu {
-            color: #${config.stylix.base16Scheme.base0B};
-            background: #${config.stylix.base16Scheme.base02};
-            font-size: 28px;
-            margin: 0px;
-            padding: 0px 30px 0px 15px;
-            border-radius: 0px 0px 40px 0px;
-          }
-          #custom-hyprbindings, #network, #battery,
-          #custom-notification, #tray, #custom-exit {
-            font-weight: bold;
-            background: #${config.stylix.base16Scheme.base0F};
-            color: #${config.stylix.base16Scheme.base00};
-            margin: 4px 0px;
-            margin-right: 7px;
-            border-radius: 10px 24px 10px 24px;
-            padding: 0px 18px;
-          }
-          #clock {
-            font-weight: bold;
-            color: #0D0E15;
-            background: linear-gradient(90deg, #${config.stylix.base16Scheme.base0E}, #${config.stylix.base16Scheme.base0C});
-            margin: 0px;
-            padding: 0px 15px 0px 30px;
-            border-radius: 0px 0px 0px 40px;
-          }
-        ''
+              border: none;
+              border-radius: 0px;
+              font-family: "JetBrainsMono Nerd Font";
+              font-weight: bold;
+              font-size: 11px;
+              min-height: 0;
+              transition: 0.3s;
+            }
+
+            window#waybar {
+                background: rgba(21, 18, 27, 0);
+                color: #cdd6f4;
+            }
+
+            tooltip {
+                background: #1e1e2e;
+                border-radius: 10px;
+                border-width: 1.5px;
+                border-style: solid;
+                border-color: #11111b;
+                transition: 0.3s;
+            }
+
+            #workspaces button {
+                padding: 5px;
+                color: #313244;
+                margin-right: 5px;
+            }
+
+            #workspaces button.active {
+                color: #a6adc8;
+            }
+
+            #workspaces button.focused {
+                color: #a6adc8;
+                background: #eba0ac;
+                border-radius: 20px;
+            }
+
+            #workspaces button.urgent {
+                color: #11111b;
+                background: #a6e3a1;
+                border-radius: 20px;
+            }
+
+            #workspaces button:hover {
+                background: #11111b;
+                color: #cdd6f4;
+                border-radius: 20px;
+            }
+
+            #custom-power_profile,
+            #custom-weather,
+            #window,
+            #clock,
+            #battery,
+            #pulseaudio,
+            #network,
+            #bluetooth,
+            #temperature,
+            #workspaces,
+            #tray,
+            #backlight {
+                background: #1e1e2e;
+                opacity: 0.8;
+                padding: 0px 10px;
+                margin: 0;
+                margin-top: 5px;
+                border: 1px solid #181825;
+            }
+
+            #temperature {
+                border-radius: 20px 0px 0px 20px;
+            }
+
+            #temperature.critical {
+                color: #eba0ac;
+            }
+
+            #backlight {
+                border-radius: 20px 0px 0px 20px;
+                padding-left: 7px;
+            }
+
+            #tray {
+                border-radius: 20px;
+                margin-right: 5px;
+                padding: 0px 4px;
+            }
+
+            #workspaces {
+                background: #1e1e2e;
+                border-radius: 20px;
+                margin-left: 5px;
+                padding-right: 0px;
+                padding-left: 5px;
+            }
+
+            #custom-power_profile {
+                color: #a6e3a1;
+                border-left: 0px;
+                border-right: 0px;
+            }
+
+            #window {
+                border-radius: 20px;
+                margin-left: 5px;
+                margin-right: 5px;
+            }
+
+            #clock {
+                color: #fab387;
+                border-radius: 20px;
+                margin-left: 5px;
+                border-right: 0px;
+                transition: 0.3s;
+                padding-left: 7px;
+            }
+
+            #network {
+                color: #f9e2af;
+                border-radius: 20px 0px 0px 20px;
+                border-left: 0px;
+                border-right: 0px;
+            }
+
+            #bluetooth {
+                color: #89b4fa;
+                border-radius: 20px;
+                margin-right: 10px
+            } 
+
+            #pulseaudio {
+                color: #89b4fa;
+                border-left: 0px;
+                border-right: 0px;
+            }
+
+            #pulseaudio.microphone {
+                color: #cba6f7;
+                border-left: 0px;
+                border-right: 0px;
+                border-radius: 0px 20px 20px 0px;
+                margin-right: 5px;
+                padding-right: 8px;
+            }
+
+            #battery {
+                color: #a6e3a1;
+                border-radius: 0 20px 20px 0;
+                margin-right: 5px;
+                border-left: 0px;
+            }
+
+            #custom-weather {
+                border-radius: 20px;
+                border-right: 0px;
+                margin-left: 0px;
+            }
+      ''
       ];
     };
   }
