7b49bd0fe0fdae0b0ed159b71fd549acdf5eb97e
installer <installer@gmail.com>
2024-07-19 17:52:31 -0500

    Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed

--- hosts/Mulder/home.nix
+++ hosts/Mulder/home.nix
@@ -186,8 +186,6 @@ in
       shellAliases = {
         sv = "sudo nvim";
         fr = "nh os switch --hostname Mulder /home/dgraham/zaneyos"; 
-        refresh-flake = "/home/dgraham/bin/flake-refresh";
-        update-flake = "/home/dgraham/bin/flake-update";
         fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
         zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
         ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
