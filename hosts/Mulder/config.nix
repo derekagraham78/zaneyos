1e1d1af0bdbaa84e38f148406ff27844e6b288c8
installer <installer@gmail.com>
2024-07-22 15:20:40 -0500

    Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed

--- hosts/Mulder/config.nix
+++ hosts/Mulder/config.nix
@@ -532,6 +532,7 @@ xdg.portal = {
     enable = true;
     wlr.enable = true;
     extraPortals = [
+    pkgs.xdg-desktop-portal-hyprland
     pkgs.xdg-desktop-portal-gtk
     pkgs.xdg-desktop-portal
     ];
