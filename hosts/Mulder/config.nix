de95912ca1cdd0461f083177a70a55bc26e8cff2
installer <installer@gmail.com>
2024-07-23 19:37:22 -0500

    Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed

--- hosts/Mulder/config.nix
+++ hosts/Mulder/config.nix
@@ -270,12 +270,17 @@
       "ddcci@" = {
         scriptArgs = "%i";
         script = ''
-            echo Trying to attach ddcci to $1
-            i=0
-            id=$(echo $1 | cut -d "-" -f 2)
+          echo Trying to attach ddcci to $1
+          i=0
+          id=$(echo $1 | cut -d "-" -f 2)
+          counter=5
+          while [ $counter -gt 0 ]; do
             if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id; then
             echo ddcci 0x37 > /sys/bus/i2c/devices/$1/new_device
+            break
           fi
+          sleep 1
+          counter=$((counter - 1))
         '';
         serviceConfig.Type = "oneshot";
       };
