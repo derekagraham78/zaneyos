a5cc3e8a700b6b3ddc2d45004908daff85090e6f
installer <installer@gmail.com>
2024-07-23 19:51:38 -0500

    Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed

--- hosts/Mulder/config.nix
+++ hosts/Mulder/config.nix
@@ -26,7 +26,7 @@
     # This is for OBS Virtual Cam Support
     kernelParams = ["reboot=acpi" "coretemp"];
     extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
-    kernelModules = ["drivetemp" "ddcci_backlight"];
+    kernelModules = ["drivetemp" "ddcci_driver"];
     # Needed For Some Steam Games
     kernel.sysctl = {
       "vm.max_map_count" = 2147483642;
